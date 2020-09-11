91edfa2d7e6ad938aa327a956793c018287f7982
tsoejima <t.soejimadeveloper@gmail.com>
2020-09-11 19:46:00 +0900

    Initial Commit

---
+++ Practice-AVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice-AVPlayer
+//
+//  Created by 副島拓哉 on 2020/09/11.
+//  Copyright © 2020 副島拓哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
